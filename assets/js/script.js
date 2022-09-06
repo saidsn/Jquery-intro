af28fe69d21069e83c5ca2a061054d2e5aeadcc4
saidsn <saidsn@code.edu.az>
2022-09-06 22:47:53 +0400

    Js-Homework

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+// $(document).ready(function () {
+
+// });
+
+// $(function () {
+
+//     let elem = document.querySelector("#products h4")
+
+//     let elem = $("#products h4");
+
+
+//     for (const item of elem) {
+//         console.log(item);
+//     }
+
+
+
+//     $("#products button").first().click(function () {
+//         console.log($(this).html('<span class = "text-danger">hello</span>'));
+//         $("#products .box").hide(800);
+//         $("#products h4").hide(800)
+
+//     })
+
+//     $("#products button").first().next().click(function () {
+//         console.log($(this).html('<span class = "text-danger">hello</span>'));
+//         $("#products h4").show(800, function () {
+//             $("#products h4").css("color", "red")
+//         })
+
+//         $("#products .box").show(800, function () {
+
+//             $("#products .box").css("background-color", "red")
+//         });
+
+
+//     })
+
+//     $("#products button").last().click(function () {
+//         console.log($(this).html('<span class = "text-danger">hello</span>'));
+//         $("#products .box,h4").toggle(800, function () {
+//             $("#products h4").css({
+//                 "color": "blue",
+//                 "font-size": "40px"
+//             })
+//         });
+
+//     })
+
+
+
+
+// })
